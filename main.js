178408bc940b284cae4f4dadf9229dc0776f9fc9
P.S.H. Gunasekara <164228734+SajanHirusha@users.noreply.github.com>
2024-04-09 02:35:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -15,3 +15,25 @@ function openNav() {
 function closeNav() {
     document.getElementById("navigation").style.width = "0";
 }
+
+// scroll reveal
+
+ScrollReveal({
+    distance:'40px',
+    duration:2000,
+    delay:200,
+});
+ScrollReveal().reveal('#myIntro1 p, #educationIntro ,.educationRow,.head,.para,.carousel-inner', { origin:'top' });
+ScrollReveal().reveal('#myIntro2,#socialMediaIcon p , #socialMediaIcon button, ', { origin:'bottom' });
+ScrollReveal().reveal('.right li,.inner1' , { origin:'left' });
+ScrollReveal().reveal('#dynamic-txts-head,.inner2' , { origin:'right' });
+
+// typed js
+
+const typed = new Typed('.dynamic-txts',{
+    strings: ['a Professional Coder.' , 'an Undergraduate','a Developer.','a Freelancer.'],
+    typeSpeed:70,
+    backSpeed:70,
+    backDelay:1000,
+    loop:true,
+});
